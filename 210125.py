665f47b5bac99aeee05d52f20780f39a4254286f
1am1337 <oskar.nitzschker@icloud.com>
2025-01-23 12:31:31 +0100

    23/25 01

--- 210125.py
+++ 210125.py
@@ -1,14 +1,16 @@
 note_names = ("C/H#", "C#/Db", "D", "D#/Eb", "E", "F/E#", "F#/Gb", "G", "G#,Ab","A", "B/A#", "H", )
 
-def _getNotenameOktave(midi):
-	print(_getNotename(midi), _getOktave(midi))
-def _getOktave(midi):
-	oktave = int(midi) // 12 -1
-	return oktave
+def _getNotenameOctave(midi):
+	print(_getNotename(midi), _getOctave(midi))
+
+def _getOctave(midi):
+	octave = int(midi) // 12 -1
+	return octave
+
 def _getNotename(midi):
 	noteNameIndex = int(midi) % 12
 	noteName = note_names[noteNameIndex]
 	return noteName
 
-_getNotenameOktave(int(input("type a midi value: ")))
+_getNotenameOctave(int(input("type a midi value: ")))
 
---
+++ uebung230125.py
@@ -0,0 +1,7 @@
+myVal = 3
+def myFunc(x: int) -> int:
+   global myVal
+   myVal += x #seiteneffekt
+   #print(myVal)
+   return x*x
+print(myFunc(42.5), myVal)
