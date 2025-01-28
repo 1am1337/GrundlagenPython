67ec8d0409004addba73cd52179c9130203545b2
1am1337 <oskar.nitzschker@icloud.com>
2025-01-28 12:46:00 +0100

    28.01.25

---
+++ listsIntro.py
@@ -0,0 +1,6 @@
+myList = []
+for i in range(20):
+	myList.append(i)
+print(myList)
+newList = [x for x in range(20)]
+print(newList)
---
+++ myModule.py
@@ -0,0 +1,6 @@
+def printThings(somethingElse = ""):
+	print("something", somethingElse)
+print(__name__)
+if __name__ == "__main__":
+	printThings("1312|161")
+
